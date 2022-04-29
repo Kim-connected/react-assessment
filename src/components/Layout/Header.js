d9e839ba770561ca64b457b266dab7e2ed567de4
Kim_codes <56189243+Kim-connected@users.noreply.github.com>
2022-04-29 13:51:57 +0200

    Add files via upload

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header =()=>{
+  return(
+    <nav className={classes.header}>
+      <h1>ASCII Store</h1>
+    </nav>
+
+  )
+}
+
+export default Header;
