d01937882dfe7483dfeeaee66a278f483dd881dc
Kim_codes <56189243+Kim-connected@users.noreply.github.com>
2022-04-29 13:49:46 +0200

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,13 @@
 import React from 'react';
+import classes from './Header.module.css';
 
-function Header() {
+const Header =()=>{
   return(
-    <div className="header">
-      <div className="header-tools-wrapper">
-        <div className="cart-wrapper">Cart</div>
-      </div>
-      <div className="header-main-content">Ascii Faces
-        <div className="sort">
-        Sort by
-          <input type="radio" value="Male" name="gender" /> Size
-          <input type="radio" value="Female" name="gender" /> Price
-          <input type="radio" value="Other" name="gender" /> ID
-        </div>
-      </div>
+    <nav className={classes.header}>
+      <h1>ASCII Store</h1>
+    </nav>
 
-    </div>
   )
-
 }
 
-export default Header
+export default Header;
