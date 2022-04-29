dd9505f48c262b31c484e0aa86b042bf41c6b86b
Kim_codes <56189243+Kim-connected@users.noreply.github.com>
2022-04-29 13:53:47 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header.js'
-import Products from './components/Products.js'
+import React, {Fragment, useState, Component} from 'react';
+import Header from './components/Layout/Header.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import List from './components/Layout/List';
+import FetchProduct from './components/Layout/FetchProduct';
 
-
-function App() {
-  return(
-    <div className="container">
-      <Header/>
-      <Products/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <Fragment>
+        <Header/>
+        <FetchProduct/>
+        <List/>
+      </Fragment>
+    );
+}
 }
 export default App;
